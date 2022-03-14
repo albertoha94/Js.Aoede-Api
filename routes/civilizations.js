c38ea5ea27ae890e26569817f1908c989daa248a
albertoha94 <albertoha94@hotmail.com>
2022-03-14 09:56:59 -0600

    Updated civilizations endpoint and data file.

--- routes/civilizations.js
+++ routes/civilizations.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+const csv = require('csv-parser');
+const results = [];
 
-router.get("/", function (req, res, next) {
-  res.send('Aqui mandaria todas las civilizaciones.');
-    
+const fileRoute = 'data/civilizations.csv';
+const fileHeaders = ['name', 'expansion', 'army_type', 'civilization_bonus'];
+
+router.get("/all", function (req, res, next) {
+  fs.createReadStream(fileRoute)
+    .pipe(csv({
+      headers: fileHeaders,
+      skipLines: 1,
+      mapValues: function ({ header, index, value }) {
+
+        // Make the civilization bonus a list.
+        var mValue = String(value).replace('//c', ",");
+        if (index == 3) {
+          var mListData = mValue.split(';');
+          return mListData;
+        } else {
+          return mValue;
+        }
+      },
+
+    }))
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+      console.log(results);
+      res.send(results);
+    });
 });
 
 module.exports = router;
