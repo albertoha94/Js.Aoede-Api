6d1f9ebb6895cd5d97239400258a8507b143163e
albertoha94 <albertoha94@hotmail.com>
2022-03-22 10:49:57 -0600

    Updated civilizations to also return a status code.

--- routes/civilizations.js
+++ routes/civilizations.js
@@ -5,30 +5,64 @@ const csv = require('csv-parser');
 const results = [];
 
 const fileRoute = 'data/civilizations.csv';
-const fileHeaders = ['name', 'expansion', 'army_type', 'civilization_bonus'];
+const fileHeaders = [
+  'name',
+  'expansion',
+  'army_type',
+  'civilization_bonus'
+];
 
-router.get("/all", function (req, res, next) {
+const valueMapper = function ({ header, index, value }) {
+  // Replace the //c with a comma (,).
+  var mValue = String(value).replace('//c', ",");
+
+  //Skip headers.
+  if (header === value) {
+    return value;
+  }
+  // Make the civilization bonus a list.
+  else if (index == 3) {
+    var mListData = mValue.split(';');
+    return mListData;
+  } else {
+    return mValue;
+  }
+};
+const onEndMapper = (itemId) => {
+  if (itemId === 'all') {
+    return {
+      status: 200,
+      data: results
+    };
+  } else {
+    var parsedId = parseInt(itemId);
+    if (isNaN(parsedId) || parsedId >= results.length) {
+      return {
+        status: 404,
+        data: 'Unable to find element with given Id.'
+      };
+    } else {
+      var item = results.at(parsedId);
+      return {
+        status: 200,
+        data: item
+      };
+    }
+  }
+};
+
+router.get("/:id", function (req, res, next) {
   fs.createReadStream(fileRoute)
     .pipe(csv({
       headers: fileHeaders,
       skipLines: 1,
-      mapValues: function ({ header, index, value }) {
-
-        // Make the civilization bonus a list.
-        var mValue = String(value).replace('//c', ",");
-        if (index == 3) {
-          var mListData = mValue.split(';');
-          return mListData;
-        } else {
-          return mValue;
-        }
-      },
-
+      mapValues: valueMapper
     }))
     .on('data', (data) => results.push(data))
     .on('end', () => {
-      console.log(results);
-      res.send(results);
+      var itemId = req.params.id;
+      var response = onEndMapper(itemId);
+      res.status(response.status).send(response.data);
     });
 });
 
